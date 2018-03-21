7c1d636bf9adafcf1363d44ce068e092592bc05c
Matthew Zipkin <pinheadmz@gmail.com>
2018-03-20 17:03:05 -0700

    Starservice

--- music.php
+++ music.php
@@ -4,6 +4,11 @@
 				<h2 style="color:white">Original Music Projects:</h2>
 				<div class="row">
 					<div class="musicItem col-md-8 blogItem">
+						<h3>Star Service</h3>
+						<i>Synth-pop / Space Opera / Electronic</i><br>
+					        <iframe width="100%" height="300" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/388421664&color=%23ff5500&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false"></iframe>
+				        </div>
+					<div class="musicItem col-md-8 blogItem">
 						<h3>The New Time</h3>
 						<i>Downtempo House / Balearic / Chillout / Trip-Hop / Electronic</i><br>
 						<iframe width="100%" height="300" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/64015187&amp;color=ff5500&amp;auto_play=false&amp;hide_related=true&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
